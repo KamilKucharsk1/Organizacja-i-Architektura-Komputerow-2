cac63f23ed21a38c6a47cc5d954dc96f5b001e27
Michal Kaczmarek <michal1110@o2.pl>
2019-05-13 03:54:07 +0200

    Added global variable change by memory, some explanation for machine language

--- main.c
+++ main.c
@@ -4,58 +4,107 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdint.h>
+#include <string.h>
 
+
+/*
+ * 1st line : 0x3b8e5894855 - our foo() in machine language
+ * 2nd line : 0xb8e5894855c35d00
+ *
+ * commands are launched from the end to the start
+ *
+ * 1st line explanation:
+ *
+ * 03 b8 - mov $0x3, %eax
+ * e5 89 48 - mov %rsp, %rbp
+ * 55 - push rbp
+ *
+ * 2nd line explanation:
+ * 00
+ * 5d - pop %rbp
+ * c3 - retq - function return
+ *
+ * (...)
+ *
+ * based on the godbolt.org
+ *
+ *
+ */
+
+int x = 5; // Global variable
+
+// ORiginal function
 int foo() {
-    printf("Calling original function!\n");
-    return 1;
+    return 3;
 }
 
+// NEw function
 int foo2(){
-    printf("Calling replaced function!\n");
     return 123;
 }
-
-int foo3(){
-    return 123;
-}
-
 
 int main() {
+
+    // Calling method before change
     printf("Value is: %d\n" ,foo());
 
+    // Pointers to method starts (getting addresses)
     int64_t *origFunc = (int64_t*)&foo;
     int64_t *newFunc = (int64_t*)&foo2;
 
-    int *f = (int*)&foo3;
+    int addressToCheck = 8; // How many address should print?
 
-    printf("p: %p | d: %p\n", origFunc, *origFunc);
-    printf("p: %p | d: %p\n", origFunc+4, *(origFunc+4));
-    printf("p: %p | d: %p\n", origFunc+8, *(origFunc+8));
-    printf("p: %p | d: %p\n", origFunc+12, *(origFunc+12));
-    printf("p: %p | d: %p\n", origFunc+16, *(origFunc+16));
-    printf("++++++");
+    printf("Foo2 data: p: %p | *p: %p\n", newFunc, *newFunc);
+
+    printf("+++++ Before changes ++++++\n");
+    for(int i=0;i<addressToCheck+1;i++){
+        printf("p: %p | *p: %p\n", origFunc+i, *(origFunc+i));
+    }
+    printf("++++++\n");
+
+    // Offset for inserting new instruction
     int32_t offset = (int64_t)newFunc - ((int64_t)origFunc + 5 * sizeof(char));
 
-    //Make the memory containing the original funcion writable
-    //Code from http://stackoverflow.com/questions/20381812/mprotect-always-returns-invalid-arguments
-    size_t pageSize = sysconf(_SC_PAGESIZE);
-    uintptr_t start = (uintptr_t)origFunc;
-    uintptr_t end = start + 1;
-    uintptr_t pageStart = start & -pageSize;
+    uintptr_t start = (uintptr_t)origFunc;  // get starting address of the original function
+    uintptr_t end = start + 1;              // get end of the original function address
 
+    size_t pageSize = sysconf(_SC_PAGESIZE); // Get size of the page in the current system
+    uintptr_t pageStart = start & -pageSize; // Calculate where the page starts
+
+    // Assign privilages for the memory to add instruction
     mprotect((void *)pageStart, end - pageStart, PROT_READ | PROT_WRITE | PROT_EXEC);
 
-    //Insert the jump instruction at the beginning of the original function
+    // Prepare jump instruction(0xe9) at the start of the original function
+    // JMP to offset << shift by 8 bits
     int64_t instruction = 0xe9 | offset << 8;
-    *origFunc = instruction;
 
-    printf("p: %p | d: %p\n", origFunc, *origFunc);
-    printf("p: %p | d: %p\n", origFunc+4, *(origFunc+4));
-    printf("p: %p | d: %p\n", origFunc+8, *(origFunc+8));
-    printf("p: %p | d: %p\n", origFunc+12, *(origFunc+12));
-    printf("p: %p | d: %p\n", origFunc+16, *(origFunc+16));
-    printf("++++++");
+    *origFunc = instruction; // Add instruction to the original function address
+
+    printf("+++++ After changes ++++++\n");
+    for(int i=0;i<addressToCheck+1;i++){
+        printf("p: %p | *p: %p\n", origFunc+i, *(origFunc+i));
+    }
+    printf("++++++\n");
     
     printf("Replaced\n");
+
+    // Calling method after change
     printf("Value is: %d\n" ,foo());
+
+    // ++++ GLOBAL VARIABLE CHANGE +++++
+
+    printf("\nGlobal variable tests\n");
+
+    int z = 999; // new value to set for global variable
+
+    // uintptr_t startX = (uintptr_t) &x; // Assign the address of the x 
+
+    printf("Value of x: %d\n", x); // Display value of x before change
+
+    // Insert data from z address into the address of x
+    memcpy((void *)&x, &z, sizeof(z));
+
+    printf("Value of x: %d\n", x); // Display new changed value
+
 }
